3937db3534d12f995ec2afc6694ba0aa3b6a1d02
Zimenko Dmytro <zimenkod@gmail.com>
2020-02-18 21:40:51 +0200

    Test Commit !

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace KernelCars
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World! From Kernel Cars");
                 });
             });
         }
